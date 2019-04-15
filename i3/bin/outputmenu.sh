b9d54db280b145b7543ad4073445e2ed0c7bd449
Zoran Melis <zoran@landofz.net>
2019-04-15 16:37:41 +0200

    Add wallpaper setup

--- i3/bin/outputmenu.sh
+++ i3/bin/outputmenu.sh
@@ -3,3 +3,7 @@
 res=$(ls $HOME/.screenlayout | cut -d. -f1 | rofi -dmenu -p "outputs")
 
 $HOME/.screenlayout/$res.sh
+
+if [ -e "$HOME/Pictures/wall.jpg" ]; then
+    xwallpaper --stretch "$HOME/Pictures/wall.jpg"
+fi
