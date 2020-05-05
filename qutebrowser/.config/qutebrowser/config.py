ec5ab4db0b39563ffd1efef85b920e94b67ebc3f
Zoran Melis <zoran@landofz.net>
2020-05-05 11:16:17 +0200

    Tighten up JS qutebrowser config, unbind `co`

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,3 +1,5 @@
+import os
+
 c.auto_save.session = True
 c.content.autoplay = False
 c.content.canvas_reading = False
@@ -8,3 +10,26 @@ c.session.lazy_restore = True
 c.tabs.background = True
 c.tabs.position = 'left'
 # c.window.hide_decoration = False
+c.completion.web_history.max_items = 10000
+
+config.unbind("co")
+config.bind("xjt", "set content.javascript.enabled true")
+config.bind("xjf", "set content.javascript.enabled false")
+
+c.content.javascript.enabled = False
+js_whitelist = [
+    "*://localhost/*",
+    "*://127.0.0.1/*",
+    "*://duckduckgo.com/*",
+    "*://startpage.com/*",
+    "*://github.com/*",
+    "*://news.ycombinator.com/*",
+]
+private_whitelist = os.path.expanduser("~/.config/qutebrowser/private-whitelist")
+if os.path.exists(private_whitelist):
+    with open(private_whitelist) as f:
+        js_whitelist += filter(lambda l: bool(l), f.read().split("\n"))
+
+for site in js_whitelist:
+    with config.pattern(site) as p:
+        p.content.javascript.enabled = True
