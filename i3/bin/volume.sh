91621f9260f84b0a8edf237f6f2c53166f789c22
Zoran Melis <zoran@landofz.net>
2020-06-04 16:51:31 +0200

    Don't hardcode audio sink/source IDs

--- i3/bin/volume.sh
+++ i3/bin/volume.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -eu
 
-sink=0
-mic=1
+sink=$(pactl list sinks short | grep "alsa_output.*analog-stereo" | cut -f1)
+mic=$(pactl list sources short | grep "alsa_input.*analog-stereo" | cut -f1)
 icon_on="\uf028"
 icon_off="\uf026"
 
@@ -15,11 +16,11 @@ function muted() {
 
 is_mic=0
 case $1 in
-	-|down) pactl set-sink-volume $sink -5% >/dev/null;;
-	+|up) pactl set-sink-volume $sink +5% >/dev/null;;
-	!|toggle) pactl set-sink-mute $sink toggle >/dev/null;;
-	%|mictoggle) pactl set-source-mute $mic toggle >/dev/null && is_mic=1;;
-	*) pactl set-sink-volume $sink $1% >/dev/null;;
+    -|down) pactl set-sink-volume $sink -5% >/dev/null;;
+    +|up) pactl set-sink-volume $sink +5% >/dev/null;;
+    !|toggle) pactl set-sink-mute $sink toggle >/dev/null;;
+    %|mictoggle) pactl set-source-mute $mic toggle >/dev/null && is_mic=1;;
+    *) pactl set-sink-volume $sink $1% >/dev/null;;
 esac
 
 if [[ $(muted) == "yes" ]]; then
