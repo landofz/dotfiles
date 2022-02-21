9f2bdd15afee1a6b506be2fdb0946d9a1be6b8f3
Zoran Melis <zoran@landofz.net>
2022-02-21 23:36:54 +0100

    Fix fzf paths

--- fzf/.config/fzf/fzf.bash
+++ fzf/.config/fzf/fzf.bash
@@ -4,9 +4,9 @@ export FZF_DEFAULT_COMMAND='ag --files-with-matches --hidden --follow'
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/lib/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/lib/fzf/src/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-[[ -s "$HOME/lib/fzf/shell/key-bindings.bash" ]] && source "$HOME/lib/fzf/shell/key-bindings.bash"
+[[ -s "$HOME/lib/fzf/src/shell/key-bindings.bash" ]] && source "$HOME/lib/fzf/src/shell/key-bindings.bash"
 bind -x '"\C-p": nvim $(fzf);'
