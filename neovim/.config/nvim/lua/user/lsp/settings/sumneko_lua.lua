bbf72650da445e2f43ddf61aef33f117a1b3465f
Zoran Melis <zoran@landofz.net>
2022-02-23 23:19:17 +0100

    Fix indentation

--- neovim/.config/nvim/lua/user/lsp/settings/sumneko_lua.lua
+++ neovim/.config/nvim/lua/user/lsp/settings/sumneko_lua.lua
@@ -1,20 +1,20 @@
 return {
-	settings = {
-		Lua = {
-			diagnostics = {
+  settings = {
+    Lua = {
+      diagnostics = {
         -- get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+        },
+      },
       -- do not send telemetry data containing a randomized but unique identifier
       telemetry = {
         enable = false,
       },
-		},
-	},
+    },
+  },
 }
