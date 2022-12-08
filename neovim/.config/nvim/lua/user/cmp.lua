418d914ed9ff2531f253d964142cfc5b6c19a039
Zoran Melis <zoran@landofz.net>
2022-12-08 13:14:16 +0100

    Upgrade cmp and lsp nvim config

--- neovim/.config/nvim/lua/user/cmp.lua
+++ neovim/.config/nvim/lua/user/cmp.lua
@@ -47,6 +47,9 @@ cmp.setup {
       luasnip.lsp_expand(args.body)
     end,
   },
+  window = {
+    documentation = cmp.config.window.bordered(),
+  },
   mapping = {
     ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<C-j>'] = cmp.mapping.select_next_item(),
@@ -121,8 +124,5 @@ cmp.setup {
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
-  },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
   },
 }
