0b033c2806e1b2926d72298f44b545070038aec6
Zoran Melis <zoran@landofz.net>
2022-12-08 13:04:35 +0100

    Use distro provided fzf config if available

--- fzf/.config/fzf/fzf.bash
+++ fzf/.config/fzf/fzf.bash
@@ -4,9 +4,19 @@ export FZF_DEFAULT_COMMAND='ag --files-with-matches --hidden --follow'
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/lib/fzf/src/shell/completion.bash" 2> /dev/null
+if [[ $- == *i* ]]; then
+    if [[ -s "/usr/share/fzf/completion.bash" ]]; then
+        source "/usr/share/fzf/completion.bash" 2> /dev/null
+    else
+        source "$HOME/lib/fzf/src/shell/completion.bash" 2> /dev/null
+    fi
+fi
 
 # Key bindings
 # ------------
-[[ -s "$HOME/lib/fzf/src/shell/key-bindings.bash" ]] && source "$HOME/lib/fzf/src/shell/key-bindings.bash"
+if [[ -s "/usr/share/fzf/key-bindings.bash" ]]; then
+    source "/usr/share/fzf/key-bindings.bash"
+else
+    [[ -s "$HOME/lib/fzf/src/shell/key-bindings.bash" ]] && source "$HOME/lib/fzf/src/shell/key-bindings.bash"
+fi
 bind -x '"\C-p": nvim $(fzf);'
