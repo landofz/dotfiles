f10d13f7657d70a4d6d9190c4bd5680415464917
Zoran Melis <zoran@landofz.net>
2023-01-16 11:27:27 +0100

    Display open-note properly on highdpi screens

--- vim/bin/open-note.sh
+++ vim/bin/open-note.sh
@@ -6,7 +6,7 @@ cd "$HOME/storage/Notebook"
 if [ -n "$WAYLAND_DISPLAY" ]; then
   file=$(fd --type file | bemenu --fn 'Hack 11' -p "notebook:" -i -l 20)
 else
-  file=$(fd --type file | rofi -dmenu -no-custom -i -p "notebook")
+  file=$(fd --type file | rofi -dmenu -no-custom -i -p "notebook" -dpi 1)
 fi
 
 [ -n "$file" ] || exit
