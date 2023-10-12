cbaa523887f5e3be6090a6c57540ecc07d022598
Zoran Melis <zoran@landofz.net>
2023-10-12 14:15:30 +0200

    Update config for indentline v3

--- neovim/.config/nvim/lua/user/indentline.lua
+++ neovim/.config/nvim/lua/user/indentline.lua
@@ -1,12 +1,10 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, ibl = pcall(require, "ibl")
 if not status_ok then
 	return
 end
 
-vim.g.indent_blankline_char = "┊"
--- vim.g.indent_blankline_char = "▏"
-vim.g.indent_blankline_use_treesitter = true
-
-indent_blankline.setup({
-	show_end_of_line = true,
+ibl.setup({
+	indent = {
+		char = "┊", -- an alternative is "▏"
+	},
 })
