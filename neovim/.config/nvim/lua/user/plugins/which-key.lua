125dd6e0bd0a3ba7f45ff42824a82ca54cfaa15e
Zoran Melis <zoran@landofz.net>
2024-09-04 22:48:16 +0200

    nvim: formatting

--- neovim/.config/nvim/lua/user/plugins/which-key.lua
+++ neovim/.config/nvim/lua/user/plugins/which-key.lua
@@ -2,15 +2,15 @@ return {
 	{
 		"folke/which-key.nvim",
 		event = "VimEnter",
-    opts = {
-      -- document key chains
-      spec = {
-        {"<leader>d", group = "[D]iagnostics"},
-        {"<leader>g", group = "[G]it"},
-        {"<leader>s", group = "[S]earch"},
-        {"<leader>v", group = "Neo[v]im"},
-        {"<leader>x", group = "E[x]tra"},
-      },
-    },
+		opts = {
+			-- document key chains
+			spec = {
+				{ "<leader>d", group = "[D]iagnostics" },
+				{ "<leader>g", group = "[G]it" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>v", group = "Neo[v]im" },
+				{ "<leader>x", group = "E[x]tra" },
+			},
+		},
 	},
 }
