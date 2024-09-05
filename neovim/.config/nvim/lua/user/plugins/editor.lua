ff06c270826097344917e03e86d197db1ab56331
Zoran Melis <zoran@landofz.net>
2024-09-05 22:58:33 +0200

    nvim: integrate autopairs and completion

--- neovim/.config/nvim/lua/user/plugins/editor.lua
+++ neovim/.config/nvim/lua/user/plugins/editor.lua
@@ -113,7 +113,18 @@ return {
 			})
 		end,
 	}, -- custom submodes and menus
-	{ "windwp/nvim-autopairs", opts = {} }, -- autoclose parens and similar constructs
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		dependencies = { "hrsh7th/nvim-cmp" },
+		config = function()
+			require("nvim-autopairs").setup({})
+			-- automatically add `(` after selecting a function or method
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	}, -- autoclose parens and similar constructs
 	{
 		"famiu/bufdelete.nvim",
 		config = function()
