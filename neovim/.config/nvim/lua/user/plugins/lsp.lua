33779cfa7e5ba3589a12c1c94b70bb265b35c360
Zoran Melis <zoran@landofz.net>
2024-09-06 17:32:05 +0200

    nvim: remove no longer needed LuaLS config

--- neovim/.config/nvim/lua/user/plugins/lsp.lua
+++ neovim/.config/nvim/lua/user/plugins/lsp.lua
@@ -183,19 +183,6 @@ return {
 				on_attach = on_attach,
 				settings = {
 					Lua = {
-						runtime = {
-							version = "LuaJIT",
-						},
-						diagnostics = {
-							-- get the language server to recognize the `vim` global
-							globals = { "vim" },
-						},
-						workspace = {
-							-- disable automatic detection and adaptation of third-party libraries
-							checkThirdParty = false,
-							-- make the server aware of Neovim runtime files
-							library = vim.api.nvim_get_runtime_file("", true),
-						},
 						-- do not send telemetry data containing a randomized but unique identifier
 						telemetry = {
 							enable = false,
