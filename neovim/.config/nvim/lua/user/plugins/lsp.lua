fc197c0362009e9aba4ef40c9f16b358a1289b60
Zoran Melis <zoran@landofz.net>
2024-09-06 17:50:03 +0200

    nvim: update deprecated API call

--- neovim/.config/nvim/lua/user/plugins/lsp.lua
+++ neovim/.config/nvim/lua/user/plugins/lsp.lua
@@ -69,7 +69,7 @@ end
 
 local function lsp_keymaps(bufnr)
 	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
 
 	local opts = { noremap = true, silent = true }
 	local map = vim.keymap.set
