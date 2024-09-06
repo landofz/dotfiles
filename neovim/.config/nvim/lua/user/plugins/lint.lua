b40a011c6bfbe17c391f30e70b63ce65ac542a6e
Zoran Melis <zoran@landofz.net>
2024-09-07 00:17:23 +0200

    nvim: add nvim-lint

---
+++ neovim/.config/nvim/lua/user/plugins/lint.lua
@@ -0,0 +1,22 @@
+return {
+	{ -- Linting
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				ansible = { "ansible_lint" },
+				javascript = { "standardjs" },
+			}
+			-- Create autocommand which carries out the actual linting
+			-- on the specified events.
+			local lint_augroup = vim.api.nvim_create_augroup("loz-lint", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
+}
