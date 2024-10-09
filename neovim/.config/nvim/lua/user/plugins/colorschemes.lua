dfb50449af49ff20c5ee57cbd603a83bdac3632c
Zoran Melis <zoran@landofz.net>
2024-10-09 10:22:24 +0200

    nvim: add melange colorscheme

--- neovim/.config/nvim/lua/user/plugins/colorschemes.lua
+++ neovim/.config/nvim/lua/user/plugins/colorschemes.lua
@@ -10,9 +10,11 @@ return {
 				},
 			})
 			vim.o.background = "dark"
+			vim.o.termguicolors = true
 			vim.cmd([[colorscheme gruvbox]])
 		end,
 	},
+	{ "savq/melange-nvim" }, -- control flow uses warm colors and data uses cold colors
 	"folke/tokyonight.nvim", -- support for lsp, treesitter and a lot of plugins, has color definitions for terminals, tmux and the like
 	{ "catppuccin/nvim", name = "catppuccin" }, -- has integrations with many plugins
 }
