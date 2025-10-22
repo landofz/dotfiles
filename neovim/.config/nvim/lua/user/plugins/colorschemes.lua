cf39c5df648f94931837a82a557df3d1b3687825
Zoran Melis <zoran@landofz.net>
2025-10-22 10:05:18 +0200

    nvim: add alabaster and flexoki colorschemes

--- neovim/.config/nvim/lua/user/plugins/colorschemes.lua
+++ neovim/.config/nvim/lua/user/plugins/colorschemes.lua
@@ -17,4 +17,6 @@ return {
 	{ "savq/melange-nvim" }, -- control flow uses warm colors and data uses cold colors
 	"folke/tokyonight.nvim", -- support for lsp, treesitter and a lot of plugins, has color definitions for terminals, tmux and the like
 	{ "catppuccin/nvim", name = "catppuccin" }, -- has integrations with many plugins
+	{ "p00f/alabaster.nvim" }, -- minimal amount of highlighting
+	{ "kepano/flexoki" }, -- inspired by analog printing inks and warm shades of paper
 }
